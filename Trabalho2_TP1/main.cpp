ff2d364f941198befb9d76d69e00b1529449adb2
Guilherme Braga <gbp1999@gmail.com>
2019-05-27 17:40:44 -0300

    deletando redundancias, pronto pra começarmos

---
+++ Trabalho2_TP1/main.cpp
@@ -0,0 +1,40 @@
+/**
+ * @file main.cpp
+ *
+ * @brief Arquivo com a implementação básica do programa.
+ *
+ * @author Gabriel e Guilherme
+ *
+ */
+
+#include "dominios.h"
+#include "dominios.cpp"
+#include "entidades.h"
+#include "entidades.cpp"
+
+#include "testes.h"
+#include "testes.cpp"
+
+
+/*
+Feito por:
+Guilherme Braga Pinto
+Gabriel Matheus da Rocha de Oliveira
+Last Update: xx/xx/2019
+UnB
+*/
+
+//Obs- Seguimos certas convenções como as que foram demonstradas em exeplos de sala de aula ao dar nome às variáveis, classes e testes.
+
+using namespace std;
+
+int main(){
+
+    system("Color 0A");
+
+    cout << "zap" << endl;
+
+
+
+    return 0;
+}
